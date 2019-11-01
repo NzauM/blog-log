96729611e4cc350d277a975163ef242c8c608425
NzauM <mercywaenu16@gmail.com>
2019-11-01 12:23:19 +0300

    Make the Git component and the navbar component

--- Documents/Nzau/Angular/Git-API/src/app/app-routing.module.ts
+++ Documents/Nzau/Angular/Git-API/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GitComponent } from './git/git.component'
+import { from } from 'rxjs';
+import { NotFoundComponent } from './not-found/not-found.component'
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {"path":"home","component":GitComponent},
+  {"pathMatch":"full","redirectTo":"home","path":""},
+  {"path":"**","component":NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Documents/Nzau/Angular/Git-API/src/app/app.module.ts
+++ Documents/Nzau/Angular/Git-API/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GitComponent } from './git/git.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitComponent,
+    NavbarComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
