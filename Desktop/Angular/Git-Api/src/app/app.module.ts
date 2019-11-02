28e471263c0369a5f0d26d79cceef575ef252723
NzauM <mercywaenu16@gmail.com>
2019-11-03 02:28:09 +0300

    Generate components for user data and their repositories

--- Desktop/Angular/Git-Api/src/app/app.module.ts
+++ Desktop/Angular/Git-Api/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { SearchUserComponent } from './search-user/search-user.component';
+import { UserDataComponent } from './user-data/user-data.component';
+import { UserReposComponent } from './user-repos/user-repos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    SearchUserComponent,
+    UserDataComponent,
+    UserReposComponent
   ],
   imports: [
     BrowserModule,
