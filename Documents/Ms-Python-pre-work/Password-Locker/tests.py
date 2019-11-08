3a6b0de062bfcea7146566952aece40a89a0112f
NzauM <mercywaenu16@gmail.com>
2019-11-08 16:15:52 +0300

    Add functions for looking if a user exists

--- Documents/Ms-Python-pre-work/Password-Locker/tests.py
+++ Documents/Ms-Python-pre-work/Password-Locker/tests.py
@@ -14,6 +14,17 @@ class TestUsers(unittest.TestCase):
         self.new_user = Users("Merciee","Mercy","Nzau","October","Mercy10")
 
 
+    def tearDown(self):
+        '''
+        The tear down method which will clean up after every test case is complete
+        '''
+
+        Users.users_list = []
+        '''
+        This will remove every test user instance after evry test case is complete
+        '''
+
+
     def test_init(self):
         '''
         To check if the user object is instantiated ocrrectly 
@@ -31,6 +42,46 @@ class TestUsers(unittest.TestCase):
         '''
         self.new_user.save_users()
         self.assertEqual(len(Users.users_list),1)
+
+    def test_save_moreThanOne_USer(self):
+        '''
+        Testcase to test whether more than one user can be added to the user list
+        '''
+
+        self.new_user.save_users()
+        test_user = Users("BeckyJ","Becka","Mbulwa","July","Becka7")
+        test_user.save_users()
+        self.assertEqual(len(Users.users_list),2)
+
+    def test_find_user_byPassword(self):
+        '''
+        To see if we can find he details of a user by using their username
+        Will help us to match users to their passwords
+        '''
+        self.new_user.save_users()
+        test_user = Users("BeckyJ","Becka","Mbulwa","July","Becka7")
+        test_user.save_users()
+
+        User_found = Users.find_user_byPassword("Becka7")
+        self.assertEqual(User_found.password,test_user.password)
+
+    def test_user_registered(self):
+        '''
+        To check if a user is already registered
+        '''
+        self.new_user.save_users()
+        test_user = Users("BeckyJ","Becka","Mbulwa","July","Becka7")
+        test_user.save_users()
+
+        user_registered = Users.user_registered("BeckyJ")
+        
+        self.assertTrue(user_registered)
+
+    
+
+
+     
+
 
 
 
--- Documents/Ms-Python-pre-work/Password-Locker/users.py
+++ Documents/Ms-Python-pre-work/Password-Locker/users.py
@@ -13,8 +13,33 @@ class Users:
 
 
     def __init__ (self,user_name,first_name,last_name,birth_month,password):
+        '''
+        This is a blueprint that every user instance must conform to
+        '''
         self.user_name = user_name
         self.first_name = first_name
         self.last_name = last_name
         self.birth_month = birth_month
         self.password = password
+
+    @classmethod
+    def find_user_byPassword(cls,password):
+        '''
+        This method will take in a password and check if the password exists to find the user
+        '''
+
+        for user in cls.users_list:
+            if user.password == password:
+                return user
+
+    @classmethod
+    def user_registered(cls,user_name):
+        '''
+        Method that checks if a user exists in the user list.Will help in authentication
+        '''
+
+        for user in cls.users_list:
+            if user.user_name == user_name:
+                return True
+            
+        return false
