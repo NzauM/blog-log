e7179d119c682735d9eb2ba644de1438818d1264
NzauM <mercywaenu16@gmail.com>
2019-11-28 10:31:54 +0300

    Add function to request quotes from API

--- Bloglog/app/models.py
+++ Bloglog/app/models.py
@@ -1,1 +1,10 @@
-class Blog
+class Quote:
+    '''
+    Quote class that defines quote objects
+    '''
+
+    def __init__(self,id,quote,author):
+        self.id = id
+        self.quote = quote
+        self.author = author
+        
--- Bloglog/app/requests.py
+++ Bloglog/app/requests.py
@@ -1,2 +1,28 @@
 from app import app
+import urllib.request,json
+from .models import Quote
+
+Quote = quote.Quote
+
+base_url = app.config["QUOTE_API_BASE_URL"]
+
+def get_quotes():
+    '''
+    Function that gets the json response to our url request
+    '''
+    with urllib.request.urlopen(base_url) as url:
+        get_quotes_data = url.read()
+        get_quotes_resp = json.loads(get_quotes_data)
+
+        quote_results = None
+
+        if get_quotes_resp['']:
+            quote_results_list = get_quotes_resp[('')]
+            quote_results = process_results(quote_results_list)
+
+    return quote_results
+
+
+
+def 
 
