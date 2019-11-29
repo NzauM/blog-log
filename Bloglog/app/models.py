410f516422c7eb4530935df49bfe38d2e424d1ad
NzauM <mercywaenu16@gmail.com>
2019-11-29 09:32:13 +0300

    Fix a bug in authentication

--- Bloglog/app/models.py
+++ Bloglog/app/models.py
@@ -36,6 +36,6 @@ class Users(UserMixin,db.Model):
     def __repr__(self):
         return f'User {self.username}'
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+    @login_manager.user_loader
+    def load_user(user_id):
+        return Users.query.get(int(user_id))
